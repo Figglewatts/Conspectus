feca029316e6edf7c3186de7a92499ee5c60b634
Figglewatts <Figglewatts@hotmail.co.uk>
2013-08-15 18:18:43 +0300

    Added TileMap.java along with some variables that will be used when drawing it

---
+++ src/figglewatts/conspectus/tile/map/TileMap.java
@@ -0,0 +1,14 @@
+package figglewatts.conspectus.tile.map;
+
+import java.awt.Graphics;
+import java.awt.Shape;
+
+public class TileMap {
+	private int mapWidth;
+	private int mapHeight;
+	
+	public void Draw(Graphics g) {
+		Shape oldClip = g.getClip();
+		
+	}
+}
